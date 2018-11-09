d19c099dfad984117acb17e80f7abce4fbb2f1b2
fruitdealer <xsincerex1990@gmail.com>
2018-11-09 16:05:02 -0500

    convering Ascii to int

---
+++ hacking/convert.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void usage(char *program_name) {
+    printf("Usage: %s <message> <# of times to repeat>\n", program_name);
+    exit(1);
+}
+
+int main(int argc, char *argv[]) {
+    int i, count;
+
+    if(argc < 3) //if fewer than 3 arguments is used 
+        usage(argv[0]); // display usage message and exit
+    
+    count = atoi(argv[2]); //convert the second arg into an  interger
+    printf("Repeating %d times\n", count);
+
+    for(i=0; i < count; i++) 
+        printf("%3d - %s\n", i, argv[1]); // print the first arg 
+    
+}
