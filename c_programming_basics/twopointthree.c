c9bf5617af29cefa58dcf27cc79e02b71ec196ce
xsincerex1990 <xsincerex1990@gmail.com>
2019-01-22 13:37:03 -0500

    warming up

---
+++ c_programming_basics/twopointthree.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//display multiple values
+int main(void) { //void as an argument because it takes none
+    int value1, value2, answer;
+    value1=10;
+    value2=20;
+    answer = value1 + value2;
+    printf("%i plus %i is equal to %i.....factzzz yo\n", value1, value2, answer);
+    return 0;
+};
+
