36237812002a8473f8cf921ac6e6850e701eabdb
xsincerex1990 <xsincerex1990@gmail.com>
2019-01-24 14:46:44 -0500

    3.3 unary & float examples

--- c_programming_basics/float&unary_minus.c
+++ c_programming_basics/float&unary_minus.c
@@ -9,6 +9,9 @@ int main(void) {
     printf("6 + a / 5 * b = %i\n", 6 + a / 5 * b); // output 16 
     printf("a / b * b = %i\n", a / b * b);// output 24 because its not a float
     printf(" c / d * d = %f\n", c / d * d); //output 25.000000
-    printf("-a = %i\n", -a);
+    printf("-a = %i\n", -a); // negated using unary minus operator which operates on a single value as opposed to binary operators which operates on
+    // two values... 
+
+    //keep in mind that int arithmetic expressions always return int and never floats
     return 0;
 }
