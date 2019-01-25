7e6d87b48fe7513fb7ab58b6c9b6da161a2ae5b8
xsincerex1990 <xsincerex1990@gmail.com>
2019-01-25 18:16:57 -0500

    buggy program

---
+++ c_programming_basics/sum_ints_chapter5_exercises.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    int num, lnum, last_result;
+    printf("ENTER A NUMBER:\n");
+    scanf("%i", &num);
+    
+    do {
+        lnum = num % 10;
+        num /= 10;
+        last_result += lnum;
+    }
+    while (num != 0);
+    printf("%i\n", last_result);
+;    return 0;
+}
