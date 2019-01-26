78561982f6a21404ed52d6edeb324f4f8497ea99
xsincerex1990 <xsincerex1990@gmail.com>
2019-01-26 17:28:56 -0500

    5.2 using if statement

---
+++ c_programming_basics/grades.c
@@ -0,0 +1,30 @@
+/* Program to calculate the average of a set of grades and count the number of failing grades*/
+
+#include <stdio.h>
+
+int main(void) {
+    int numberofgrades, i, grade;
+    int gradeTotal = 0;
+    int fails = 0;
+    float average;
+
+    printf("How many grades will you be entering? ");
+    scanf("%i", &numberofgrades);
+
+    for (i=1; i<=numberofgrades; ++i) {
+        printf("Enter grade #%i: ", i);
+        scanf("%i", &grade);
+
+        gradeTotal += grade;
+
+        if (grade < 65)
+        ++fails;
+    }
+
+    average = (float) gradeTotal / numberofgrades;
+
+    printf("\nGrade average = %.2f\n", average);
+    printf("Number of failures = %i\n", fails);
+
+    return 0;
+}
